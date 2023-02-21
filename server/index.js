724ea586ac16686cb08029e8117f62ba02f73d33
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2023-02-21 23:46:29 +0530

    connected mongodb database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
@@ -15,10 +16,12 @@ app.get("/", async (req, res) => {
 });
 
 const startServer = async () => {
-  app.listen(
-    8080,
-    console.log("Server has started on port http://localhost:8080")
-  );
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
